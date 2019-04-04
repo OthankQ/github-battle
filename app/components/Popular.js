7c51b8238b3570be07590dc309afd660974687b7
Ryan <kim1885@purdue.edu>
2019-04-04 11:02:02 -0500

    fix props.repos issue

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,61 +1,67 @@
 var React = require('react');
 var PropTypes = require('prop-types');
-var api = require('../utils/api')
+var api = require('../utils/api');
+
 
 // State
 // Lifecycle
 // UI
 
+
 // Stateless functional component
 function SelectLanguage (props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-  
-  return(
-    <ul className='languages'>
-          {languages.map(function(lang) {
-            return (
-              <li
-                // When a language is selected, highlight it 
-                style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
-                onClick={props.onSelect.bind(null, lang)}
-                key={lang}>
-                  {lang}
-              </li>
-            )
-          })}
-        </ul>
-  )
-}
-
-SelectLanguage.propType = {
-  selectedLanguage: PropTypes.string.isRequired,
-  onSelect: PropTypes.func.isRequired,
-}
-
-function RepoGrid (props) {
   return (
-    <ul className='popular-list'>
-      {props.map(function(repo){
-        return
+    <ul className='languages'>
+      {languages.map(function (lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+              {lang}
+          </li>
+        )
       })}
     </ul>
   )
 }
 
+function RepoGrid (props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function (repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
 class Popular extends React.Component {
-    // Initial state
+  // Initial state
   constructor(props) {
     super();
     this.state = {
       selectedLanguage: 'All',
       repos: null,
     };
+
     // So that updateLanguge gets invoked with the expected context. Binding 'this' to this component instance.
     this.updateLanguage = this.updateLanguage.bind(this);
   }
-
-  componentDidMount () {
-    this.updateLanguage(this.state.selectedLanguage);
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage)
   }
 
   // A way to update the state
@@ -63,29 +69,30 @@ class Popular extends React.Component {
     this.setState(function () {
       return {
         selectedLanguage: lang,
-        repos: null,
+        repos: null
       }
     });
 
     api.fetchPopularRepos(lang)
-    .then(function(repos){
-      this.setState(function(){
-        return{
-          repos: repos,
-        }
-      })
-    }.bind(this));
+      .then(function (repos) {
+        this.setState(function () {
+          return {
+            repos: repos
+          }
+        });
+      }.bind(this));
   }
-
+  
   // UI
   render() {
     return (
       <div>
-        <SelectLanguage 
+        <SelectLanguage
           selectedLanguage={this.state.selectedLanguage}
-          onSelect={this.updateLanguage}
-        />
-        <RepoGrid repos={this.state.repos}/>
+          onSelect={this.updateLanguage} />
+        {!this.state.repos
+          ? <p>LOADING!</p>
+          : <RepoGrid repos={this.state.repos} />}
       </div>
     )
   }
